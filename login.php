5444a94bb795fdc6fc30d3ee8398593e1a3e9fc3
Nathan <nathanvcappellen@solcon.nl>
2025-12-16 14:49:42 +0100

    footer ging niet helemaal lekker

--- login.php
+++ login.php
@@ -1,22 +1,35 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transportapplicatie</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
 <?php
-
-include("inc/header.php");
+    include 'inc/header.php';
 ?>
-<link rel="stylesheet" type="text/css" href="css/style.css"> 
-<main class="main-content">
-    <div id="login"><h2>Transportapplicatie</h2></div>
-    <div id="login">
-        <div>
-            <form action="authorisatie.php" method="POST" class="frmlogin">
-                <label for="fInlog">Inlognaam:</label><input type="text" name="inlognaam" id="fInlog" size="25" placeholder="inlognaam..."><br>
-                <label for="fWachtwoord">Wachtwoord:</label><input type="password" id="fWachtwoord" name="wachtwoord" size="25" placeholder="wachtwoord..."><br>
-                <input type="submit" name="submit" value="login"><br>
-            </form>
+<body>
+    <main class="main-content">
+        <div id="login"><h2>Transportapplicatie</h2></div>
+        <div id="login">
+            <div>
+                <form action="authorisatie.php" method="POST" class="frmlogin">
+                    <label for="fInlog">Inlognaam:</label><input type="text" name="inlognaam" id="fInlog" size="25" placeholder="inlognaam..."><br>
+                    <label for="fWachtwoord">Wachtwoord:</label><input type="password" id="fWachtwoord" name="wachtwoord" size="25" placeholder="wachtwoord..."><br>
+                    <input type="submit" name="submit" value="login"><br>
+                </form>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
+    <style>
+        main {
+            height: 80vh;
+        }
+    </style>
+</body>
 
 <?php
-
-include ("inc/footer.php");
+    include 'inc/footer.php';
 ?>
+</html>
