2634a81160c1f7822c1ae766a7399c44e004f818
Nathan <nathanvcappellen@solcon.nl>
2026-04-01 09:00:30 +0200

    x

--- inc/database.php
+++ inc/database.php
@@ -3,12 +3,17 @@
 // Database connectie
 define('HOST', 'localhost');
 define('DATABASE', 'transportbedrijf');
-define('USER', 'mutationuser');
-define('PASSWORD', '1234');
+define('USER', 'root');
+define('PASSWORD', '');
 $dbconn = '';
 
 try {
-    $dbconn = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE . ";charset=utf8mb4", USER, PASSWORD);
-} catch(exception $e) {
-    $dbconn = $e;
+    $dbconn = new PDO(
+        "mysql:host=" . HOST . ";dbname=" . DATABASE . ";charset=utf8mb4",
+        USER,
+        PASSWORD
+    );
+    $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connectie mislukt: " . $e->getMessage());
 }
